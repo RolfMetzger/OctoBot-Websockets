c7f516d929e6cf92db1a032062a04ebe6d0557d5
Herklos <herklos.dev@protonmail.com>
2019-10-05 20:00:08 +0200

    Added first test

--- tests/data/test_book.py
+++ tests/data/test_book.py
@@ -13,3 +13,20 @@
 #
 #  You should have received a copy of the GNU Lesser General Public
 #  License along with this library.
+from octobot_websockets.data.book import Book
+
+
+def test_create_book():
+    book = Book()
+    assert not book.asks
+    assert not book.bids
+    assert book.timestamp == 0
+
+
+def test_update_book():
+    book = Book()
+    book.handle_book_update([[100.3, 0.112504], [100.5, 0.484882], [100.1, 2.0]],
+                            [[100.5, 0.126516], [101.01, 0.49906]])
+    assert book.timestamp != 0
+    assert book.asks
+    assert book.bids
